4f23380df27c0e2018d1df074572d7e26d91ed64
Mickael <mickael.n.xd@gmail.com>
2021-10-31 23:34:29 -0300

    Added Dashboard

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,65 @@
+import 'package:bytebank/screens/contacts_list.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/images/bytebank_logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                color: Theme.of(context).primaryColor,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => ContactsList(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    height: 100,
+                    width: 150,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.people,
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .bodyText1
+                                ?.color,
+                          ),
+                          Text(
+                            'Contacts',
+                            style: TextStyle(
+                              color: Theme.of(context)
+                                  .primaryTextTheme
+                                  .bodyText1
+                                  ?.color,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
