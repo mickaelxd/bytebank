c361c691f9eadf65d4b73abce1b6d98f0f5b9eb2
Mickael <mickael.n.xd@gmail.com>
2021-11-05 17:56:49 -0300

    Added Firebase to Flutter Deps

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:bytebank/screens/dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(BytebankApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  runApp(BytebankApp());
+}
 
 class BytebankApp extends StatelessWidget {
   @override
